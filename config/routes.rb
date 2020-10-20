6b4f58a61e0d1aa32ad8b98c58d20e7db53b557d
Matthew Gariepy <mgariepy@aceproject.space>
2020-10-20 10:37:50 -0500

    Created the Artists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+  get 'artists/show'
   get 'songs/index'
   get 'songs/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
