e71d1790ed4a1590efdbec7ddac7cac492618834
Matthew Gariepy <mgariepy@aceproject.space>
2020-10-20 14:06:29 -0500

    Index and Show views now display all data

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,9 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.includes(:artist, :genre).order("popularity DESC")
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 end
