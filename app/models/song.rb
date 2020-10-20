e2a1614e613cddbf668dcb754cfccd8f6f1d6baa
Matthew Gariepy <mgariepy@aceproject.space>
2020-10-20 10:36:22 -0500

    Created the Songs controller

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ApplicationRecord
   belongs_to :artist
   belongs_to :genre
   validates :title, :year, presence: true
-  validates :year, :bpm, :energy, :danceability, :loudness, :liveness, :valence, :duration, :acousticness, :speechiness, :popularity, numericality: { only_integer: true }
+  validates :year, :bpm, :energy, :danceability, :loudness, :liveness, :valence, :duration, :acousticness, :speechness, :popularity, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,45 @@
 require 'csv'
 
+Song.delete_all
 Artist.delete_all
 Genre.delete_all
-Song.delete_all
+
+puts "Total: #{Artist.count} artists"
+puts "Total: #{Genre.count} genres"
+puts "Total: #{Song.count} songs"
 
 filename = Rails.root.join('db/top10s.csv')
 csv_data = File.read(filename)
 songs = CSV.parse(csv_data, headers: true)
+
+songs.each do |m|
+  artist = Artist.find_or_create_by(name: m['artist'])
+  genre = Genre.find_or_create_by(name: m['top genre'])
+
+  if artist && artist.valid? && genre && genre.valid?
+    genreId = Genre.find_by name: m['top genre']
+    song = artist.songs.create(
+      title: m['title'],
+      year: m['year'],
+      bpm: m['bpm'],
+      energy: m['nrgy'],
+      danceability: m['dnce'],
+      loudness: m['dB'],
+      liveness: m['live'],
+      valence: m['val'],
+      duration: m['dur'],
+      acousticness: m['acous'],
+      speechness: m['spch'],
+      popularity: m['pop'],
+      genre_id: genreId.id
+    )
+    puts "Invalid Song #{m['title']}" unless song.valid?
+  else
+    puts "Invalid Artist or Genre #{m['artist']}, #{m['top genre']}"
+  end
+end
+
+puts "Created #{Artist.count} artists"
+puts "Created #{Genre.count} genres"
+puts "Created #{Song.count} songs"
+
