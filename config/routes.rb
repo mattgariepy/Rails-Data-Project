04f2e3f76f9d16a7bc7f67d2b665a45c426c8df8
Matthew Gariepy <mgariepy@aceproject.space>
2020-10-20 11:35:30 -0500

    Added Restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'genres/index'
-  get 'genres/show'
-  get 'artists/index'
-  get 'artists/show'
-  get 'songs/index'
-  get 'songs/show'
+  # get 'genres/index'
+  # get 'genres/show'
+  # get 'artists/index'
+  # get 'artists/show'
+  # get 'songs/index'
+  # get 'songs/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :songs, only: [:index, :show]
+  resources :artists, only: [:index, :show]
+  resources :genres, only: [:index, :show]
 end
